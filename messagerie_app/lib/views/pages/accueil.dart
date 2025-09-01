3f69d8c89b5314cdad6432da6b37f01154d3055b
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-09-01 11:25:29 +0200

    feat: Ajout d'un text Field dans la page de message

--- messagerie_app/lib/views/pages/accueil.dart
+++ messagerie_app/lib/views/pages/accueil.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:messagerie_app/data/notifiers.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+  
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      counterNotifier.value++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
+      
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
+              '${counterNotifier.value}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
